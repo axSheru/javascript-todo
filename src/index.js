9e4899d61069c918f47b892a099767ea2a53b3b6
Alex Paredes <alexparedesr95@gmail.com>
2022-02-03 22:01:57 -0600

    FEAT: Deleted hardcoded TODO.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,3 @@ import { Todo, TodoList } from './classes';
 import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
-
-const tarea = new Todo( 'Tomar medicina.' );
-
-todoList.nuevoTodo( tarea );
-
-console.log( todoList );
-
-crearTodoHtml( tarea );
