584777ad99af822e8721b28589a189099c703040
Alex Paredes <alexparedesr95@gmail.com>
2022-02-04 21:28:28 -0600

    FEAT: Paint todos in HTML.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import { Todo, TodoList } from './classes';
 import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
+
+// todoList.todos.forEach( todo => crearTodoHtml( todo ) );
+// Forma simplificada, si se maneja solo un argumento se manda por default a la función.
+todoList.todos.forEach( crearTodoHtml );
